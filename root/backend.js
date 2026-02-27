7321c13010d5aac8277b8f6941b5aae0d89b6e47
ottoheiskanen <ottoheiskanen@gmail.com>
2026-02-27 17:43:53 +0200

    repo clean after turning it into lib

--- root/backend.js
+++
@@ -1,9 +0,0 @@
-function backendFunction(){
-  return "this is a secret backend function";
-}
-
-function printObject(object) {
-  console.table(object);
-}
-
-module.exports = {backendFunction, printObject};
